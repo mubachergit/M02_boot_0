85efc5a4a49fc9075da9a91523e7db821c6c812d
Mubacher Alrifai Jandali <mubacheralrifai@gmail.com>
2020-04-22 08:20:07 +0200

    Funciones clasicas

--- p1.py
+++ p1.py
@@ -1,8 +1,17 @@
-def sumaTodo(limitTo):
+def sumaTodos(limitTo):
     resultado = 0
     for i in range(0, limitTo+1):
         resultado += i
         
     return resultado
 
-print(sumaTodo(100))
+
+def sumaTodosLosCuadrados(limitTo):
+    resultado = 0
+    for i in range (limitTo + 1):
+        resultado += i*i
+    
+    return resultado
+        
+print(sumaTodos(100))
+print(sumaTodosLosCuadrados(3))
