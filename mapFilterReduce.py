502f29da0abc8600147c861fcd91daafb89d8efe
Mubacher Alrifai Jandali <mubacheralrifai@gmail.com>
2020-04-22 12:29:41 +0200

    FiltroLambda

---
+++ mapFilterReduce.py
@@ -0,0 +1,7 @@
+lista = [1, 3, -1, 15, 9 ]
+
+listaDobles = map(lambda x : x*2, lista)
+
+listaPares = filter(lambda x: x % 2 == 0, lista)
+
+listaMultiplosdetres = filter(lambda x: x % 3 == 0, lista)
