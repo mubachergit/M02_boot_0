cdb47c6f247bb0d4c58322939cd16853e10ea011
mubacher <mubacheralrifai@gmail.com>
2020-04-22 18:22:43 +0200

    map, filter, reduce

--- mapFilterReduce.py
+++ mapFilterReduce.py
@@ -1,7 +1,26 @@
+from functools import reduce
+
 lista = [1, 3, -1, 15, 9 ]
 
 listaDobles = map(lambda x : x*2, lista)
 
-listaPares = filter(lambda x: x % 2 == 0, lista)
+def esPar(x):
+    return x % 2 == 0
 
-listaMultiplosdetres = filter(lambda x: x % 3 == 0, lista)
+listaPares = filter(lambda x: x % 2 == 0, lista)
+listaPares1 = filter(esPar, lista)
+
+sumatorio = reduce(lambda x, y: x + y, lista)
+
+suma100 = reduce(lambda x, y: x + y, range(101))
+
+sumatorioDobles = reduce(lambda x, y : x + y*2, lista)
+
+
+print(list(listaPares))
+print(list(listaPares1))
+
+print(sumatorio)
+print(sumatorioDobles)
+
+print(suma100)
